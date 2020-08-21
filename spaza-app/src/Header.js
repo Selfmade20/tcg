cd616ec242d2983c3ab4d46620e6351d443fc8ed
Selfmade20 <tebogo1686@gmail.com>
2020-08-21 10:57:30 +0200

    finalizing nav component

--- spaza-app/src/Header.js
+++ spaza-app/src/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -20,22 +21,29 @@ function Header() {
       <div className="header__nav">
         <Link to="/login" className="header__link">
           <div className="header__option">
-            <span>Hello Tebogo</span>
-            <span>Sign in</span>
+            <span className="header__optionLineOne">Hello Tebogo</span>
+            <span className="header__optionLineTwo">Sign in</span>
           </div>
         </Link>
 
         <Link to="/" className="header__link">
           <div className="header__option">
-            <span>Returns</span>
-            <span>& Orders</span>
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
           </div>
         </Link>
 
         <Link to="/" className="header__link">
           <div className="header__option">
-            <span>Your</span>
-            <span>Prime</span>
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">0</span>
           </div>
         </Link>
       </div>
