e99a097777431a710af7c551cbc6300dd1e48849
Selfmade20 <tebogo1686@gmail.com>
2020-08-21 10:24:53 +0200

    navbar styling

--- spaza-app/src/App.js
+++ spaza-app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './Header'
 
 function App() {
   return (
@@ -8,12 +9,13 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
-            <h1>Checkout</h1>
+            <h1>Checkout</h1> 
           </Route>
           <Route path="/login">
             <h1>Login Page</h1>
           </Route>
           <Route path="/">
+            <Header />
             <h1>Home Page!!!</h1>
           </Route>
         </Switch>
