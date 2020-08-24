c2848c3142a0c4e6c27c4d7f4b2dd6f3ca6ccdc9
Selfmade20 <tebogo1686@gmail.com>
2020-08-24 12:40:46 +0200

    created getBasketTotal selctor

--- spaza-app/src/redux/reducer.js
+++ spaza-app/src/redux/reducer.js
@@ -1,35 +1,13 @@
 export const initialState = {
-  basket: [
-    {
-      id: "2",
-      title: "Playstation 5",
-      price: 180,
-      rating: 4,
-      image:
-        "https://o.aolcdn.com/images/dims?resize=1200%2C630&crop=1200%2C630%2C0%2C0&quality=95&image_uri=https%3A%2F%2Fs.yimg.com%2Fos%2Fcreatr-uploaded-images%2F2020-06%2F65068ac0-acbf-11ea-b9fb-ee297695f454&client=amp-blogside-v2&signature=685370e53cbcbb00a28cd0d1b29be2585964ca57",
-    },
-    {
-      id: "2",
-      title: "Playstation 5",
-      price: 180,
-      rating: 4,
-      image:
-        "https://o.aolcdn.com/images/dims?resize=1200%2C630&crop=1200%2C630%2C0%2C0&quality=95&image_uri=https%3A%2F%2Fs.yimg.com%2Fos%2Fcreatr-uploaded-images%2F2020-06%2F65068ac0-acbf-11ea-b9fb-ee297695f454&client=amp-blogside-v2&signature=685370e53cbcbb00a28cd0d1b29be2585964ca57",
-    },
-    {
-      id: "2",
-      title: "Playstation 5",
-      price: 180,
-      rating: 4,
-      image:
-        "https://o.aolcdn.com/images/dims?resize=1200%2C630&crop=1200%2C630%2C0%2C0&quality=95&image_uri=https%3A%2F%2Fs.yimg.com%2Fos%2Fcreatr-uploaded-images%2F2020-06%2F65068ac0-acbf-11ea-b9fb-ee297695f454&client=amp-blogside-v2&signature=685370e53cbcbb00a28cd0d1b29be2585964ca57",
-    },
-  ],
+  basket: [],
   user: null,
 };
 
+export const getBasketTotal = (basket) =>
+  basket.reduce((amount, item) => item.price + amount, 0);
+
 export default function reducer(state, action) {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
     case "ADD_TO_BASKET":
       // adding items to basket
@@ -44,8 +22,7 @@ export default function reducer(state, action) {
 
       if (index >= 0) {
         newBasket.splice(index, 1);
-      }else {
-
+      } else {
       }
 
       return { ...state, basket: newBasket };
