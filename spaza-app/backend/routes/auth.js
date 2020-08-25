20343e49dd15902ff7ef964f23562211f1c84434
Selfmade20 <tebogo1686@gmail.com>
2020-08-25 15:03:07 +0200

    registration validation

---
+++ spaza-app/backend/routes/auth.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const User = require("../model/User");
+
+const Joi = require("@hapi/joi");
+
+const schema = {
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+};
+
+router.post("/register", async (req, res) => {
+  const { error } = Joi.validate(req.body, schema);
+  if (error) return res.status(400).send(error.details[0].message);
+
+    const user = new User({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    try {
+      const savedUser = await user.save();
+      res.send(savedUser);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+});
+
+module.exports = router;
