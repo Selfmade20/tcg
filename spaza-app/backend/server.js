21cbde84236c6180a6799f9e34f156b75a741413
Selfmade20 <tebogo1686@gmail.com>
2020-08-25 13:34:58 +0200

    created database connection string

--- spaza-app/backend/server.js
+++ spaza-app/backend/server.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.DATABASE_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => {
+    console.log("Successfully connected to Mongodb database!");
+  }
+);
+const authRoute = require("./routes/auth");
+
+app.use("/user", authRoute);
 
 app.listen(5000, () => console.log("Server Up and Running!"));
