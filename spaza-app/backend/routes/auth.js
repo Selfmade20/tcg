2913e93106537296e7bab0a7b70c47c0eb0de2c2
Selfmade20 <tebogo1686@gmail.com>
2020-08-26 08:25:45 +0200

    validation and hashing

--- spaza-app/backend/routes/auth.js
+++ spaza-app/backend/routes/auth.js
@@ -1,27 +1,24 @@
 const router = require("express").Router();
 const User = require("../model/User");
-
-const Joi = require("@hapi/joi");
-
-const schema = {
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-};
+const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
-  const { error } = Joi.validate(req.body, schema);
+  const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-    const user = new User({
-      email: req.body.email,
-      password: req.body.password,
-    });
-    try {
-      const savedUser = await user.save();
-      res.send(savedUser);
-    } catch (error) {
-      res.status(400).send(error);
-    }
+  const emailExists = await User.findOne({ email: req.body.email });
+  if(emailExists) return res.status(400).send("Email already exists")
+
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;
