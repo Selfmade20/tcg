1e652ad57eb38aa73fbe3e0685739301099aa507
Selfmade20 <tebogo1686@gmail.com>
2020-08-26 11:51:06 +0200

    setting up redux store

---
+++ spaza-app/src/store/authReducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+    user: null,
+    isAuthorized: false,
+    error: { error: null }
+  };
+  
+  const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case "AUTH_SAVE_USER":
+        return { ...state, user: action.payload };
+      case "CHANGE_AUTHORIZATION":
+        return { ...state, isAuthorized: action.payload }
+      case "LOGOUT_USER":
+        return { ...state, user: null }
+      case "AUTH_ERROR":
+        return { ...state, error: action.payload }
+      default:
+        return state;
+    }
+  };
+  
+  export default authReducer;
