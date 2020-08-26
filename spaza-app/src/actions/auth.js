1024a4134482dd93c987b6a363cd8481a2224651
Selfmade20 <tebogo1686@gmail.com>
2020-08-26 16:08:55 +0200

    using window.reload for actions

--- spaza-app/src/actions/auth.js
+++ spaza-app/src/actions/auth.js
@@ -1,9 +1,16 @@
 import axios from "axios";
+import createBrowserHistory from "../history/index";
 
 export const register = (registerDetails) => {
+  const registerReloadWindow = () => {
+    window.location.reload();
+  };
+
   return async () => {
     try {
       await axios.post("http://localhost:5000/user/register", registerDetails);
+      createBrowserHistory.push("/home");
+      registerReloadWindow();
     } catch (error) {
       console.log(error);
     }
@@ -11,9 +18,15 @@ export const register = (registerDetails) => {
 };
 
 export const login = (loginDetails) => {
+  const loginReloadWindow = () => {
+    window.location.reload();
+  };
+
   return async () => {
     try {
       await axios.post("http://localhost:5000/user/login", loginDetails);
+      createBrowserHistory.push("/register");
+      loginReloadWindow();
     } catch (error) {
       console.log(error);
     }
