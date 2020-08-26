3eeb86bfb4b44fe33354feae604e6c4511b59ef5
Selfmade20 <tebogo1686@gmail.com>
2020-08-26 13:44:38 +0200

    .

--- spaza-app/src/store/rootReducer.js
+++ spaza-app/src/store/rootReducer.js
@@ -1,7 +1,6 @@
-import {combineReducers} from 'redux';
-import authReducer from './authReducer';
+import { combineReducers } from "redux";
+import authReducer from "./authReducer";
 
 export const rootReducer = combineReducers({
-    auth: authReducer
-})
-
+  auth: authReducer,
+});
