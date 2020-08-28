8b44a424d052ce9a3b70ba1bc25f742f8a879287
Selfmade20 <tebogo1686@gmail.com>
2020-08-28 12:19:51 +0200

    fetching weather data from api

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const api = {
@@ -7,11 +7,47 @@ const api = {
 };
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (event) => {
+    if (event.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result)
+          setQuery('')
+          console.log(weather)
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let date = String(new window.Date());
+    date = date.slice(3, 15);
+    return `${date}`;
+  };
+
   return (
     <div className="app">
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Search...." />
+          <input
+            type="text"
+            className="search-bar"
+            value={query}
+            onKeyPress={search}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search...."
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">16°c</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
