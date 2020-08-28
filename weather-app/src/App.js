4aed5db6cc8634418d0497db895d78a65b12f70d
Selfmade20 <tebogo1686@gmail.com>
2020-08-28 10:15:36 +0200

    initialized an api key for weather data

---
+++ weather-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+const api = {
+  key: "2a8e4f918c3b02125cb8594fb357cbdd",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello App day!</h1>
+    </div>
+  );
+}
+
+export default App;
