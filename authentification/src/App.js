83ed39cef6391e785cede8e6ac6384c9789fadad
Selfmade20 <tebogo1686@gmail.com>
2020-09-02 13:13:46 +0200

    Creating my front-end routes

--- authentification/src/App.js
+++ authentification/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
