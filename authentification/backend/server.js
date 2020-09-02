cf7d409766a471d0e781b23ebd4e806bee54804b
Selfmade20 <tebogo1686@gmail.com>
2020-09-02 13:06:10 +0200

    setting up server to use routes

--- authentification/backend/server.js
+++ authentification/backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const authRoute = require("./routes/auth");
 
 app.use(express.json);
 app.use(cors());
 
-app.listen(5000, () => console.log("Server up and running"))
+app.use("/", authRoute);
+
+app.listen(5000, () => console.log("Server up and running"));
