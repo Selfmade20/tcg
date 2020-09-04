150a3ca90ed589fe4d323cfbaa443b012222135d
Selfmade20 <tebogo1686@gmail.com>
2020-09-04 12:11:37 +0200

    creating component routes

--- registration-app/src/App.js
+++ registration-app/src/App.js
@@ -3,15 +3,23 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import { NavigationBar } from "./components/Navbar";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
-          <Route path="/login">
+          <Route exact path="/">
             <NavigationBar />
+            <Register />
+          </Route>
+          <Route path="/login">
             <Login />
+          </Route>
+          <Route path="/profile">
+            <Profile />
           </Route>
         </Switch>
       </div>
