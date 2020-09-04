f08026251956a11157ddd2c6c7aac4c6cfd2b263
Selfmade20 <tebogo1686@gmail.com>
2020-09-04 13:18:21 +0200

    adding register method

--- registration-app/src/components/Register.js
+++ registration-app/src/components/Register.js
@@ -1,34 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.css";
+import { register } from "../actions/auth";
 
 function Registration() {
-  const [data, setData] = useState({
+  const [registerDetails, setRegisterDetails] = useState({
     name: "",
     surname: "",
     email: "",
-    idNumber: "",
+    IDnumber: "",
     address: "",
   });
 
+  const submit = (event) => {
+    event.preventDefault();
+  };
+  console.log( submit);
+
   const handleChange = (event) => {
     event.preventDefault();
-    
+    setRegisterDetails({
+      ...registerDetails,
+      [event.target.name]: event.target.value,
+    });
   };
+
   return (
     <div className="register">
       <div className="register__container">
         <h1>Sign up</h1>
-        <form>
+        <form onSubmit={(e) => submit(e)}>
           <h5>Name</h5>
-          <input type="text" name="name" placeholder="Name" />
+          <input
+            type="text"
+            name="name"
+            value={registerDetails.name}
+            placeholder="Name"
+            onChange={handleChange}
+          />
           <h5>Surname</h5>
-          <input type="surname" placeholder="Surname" name="surname" />
+          <input
+            type="surname"
+            placeholder="Surname"
+            name="surname"
+            value={registerDetails.surname}
+            onChange={handleChange}
+          />
           <h5>Email</h5>
-          <input type="email" placeholder="Email" name="email" />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={handleChange}
+            value={registerDetails.email}
+          />
           <h5>ID Number</h5>
-          <input type="id" placeholder="ID Number" name="id" />
+          <input
+            type="text"
+            placeholder="ID Number"
+            name="text"
+            onChange={handleChange}
+            value={registerDetails.IDnumber}
+          />
           <h5>Address</h5>
-          <input type="address" placeholder="Address" name="address" />
+          <input
+            type="address"
+            placeholder="Address"
+            name="address"
+            value={registerDetails.address}
+            onChange={handleChange}
+          />
           <button type="submit">Sign up</button>
         </form>
       </div>
