b3633f54abd059d620818cc66cc6d0cd63680a9e
Selfmade20 <tebogo1686@gmail.com>
2020-09-04 12:09:17 +0200

    get user profile component

---
+++ registration-app/src/components/Profile.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const api = {
+  key: "http://localhost:4000/",
+};
+
+function Profile() {
+  const [data, setData] = useState({});
+
+  const getData = async (event) => {
+    await fetch(`${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+        console.log(result);
+      });
+  };
+
+  return (
+    <ul>
+      <button onClick={getData}>Get Data</button>
+    </ul>
+  );
+}
+
+export default Profile;
