d07d544af3b04e6a65f33874281f845c33c23ddd
Selfmade20 <tebogo1686@gmail.com>
2020-09-04 11:12:41 +0200

    added a get user route

--- registration-app/backend/routes/auth.js
+++ registration-app/backend/routes/auth.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
+router.route("/").get((req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error:" + err));
+});
+
 router.post("/register", async (req, res) => {
   const user = new User({
     name: req.body.name,
