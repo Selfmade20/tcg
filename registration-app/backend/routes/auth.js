d6d8dd88c0500c2cf74c9b141a0eb897fcd9fcb7
Selfmade20 <tebogo1686@gmail.com>
2020-09-04 14:14:52 +0200

    added login route

--- registration-app/backend/routes/auth.js
+++ registration-app/backend/routes/auth.js
@@ -24,4 +24,13 @@ router.post("/register", async (req, res) => {
   }
 });
 
+
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email is incorrect");
+
+  res.send("Logged in");
+});
+
+
 module.exports = router;
