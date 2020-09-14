7ddf7a75fda3d94c4cf2f2f191aa6ab30e441701
Selfmade20 <tebogo1686@gmail.com>
2020-09-14 09:51:57 +0200

    capture user details route

---
+++ contact-me/backend/routes/auth.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const User = require("../model/User");
+
+router.post("/capture", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    surname: req.body.surname,
+    mobileNumber: req.body.mobileNumber,
+    email: req.body.email,
+    question: req.body.question,
+  });
+});
+
+try {
+  const savedInfo = await user.save();
+  res.send(savedInfo);
+} catch (error) {
+  res.status(400).send(error);
+}
+
+module.exports = router;
