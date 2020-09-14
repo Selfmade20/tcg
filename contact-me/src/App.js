aad6c080c64c5f7b758e19bae07d9bee07d68aaf
Selfmade20 <tebogo1686@gmail.com>
2020-09-14 13:12:43 +0200

    importing my components into routes

--- contact-me/src/App.js
+++ contact-me/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Home from './components/Home'
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route exact path="/">
+            <Home />
           </Route>
         </Switch>
       </div>
