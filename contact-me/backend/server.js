2efcf8333e7a74fd32ee54383669c5172fa8ca31
Selfmade20 <tebogo1686@gmail.com>
2020-09-14 10:07:00 +0200

    mongodb database connection

--- contact-me/backend/server.js
+++ contact-me/backend/server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.DATABASE,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => {
+    console.log("Successfully connected to Mongodb database!");
+  }
+);
 
 app.use(express.json());
 app.use(cors());
+app.use("/", authRoute);
 
 app.listen(4000, () => console.log("Server Up and Running!"));
