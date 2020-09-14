4aa60ae19acba73827fa8fe8567e1808cd0bcce1
Selfmade20 <tebogo1686@gmail.com>
2020-09-14 13:11:45 +0200

    Added a fetch for company details

---
+++ contact-me/src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "./Home.css";
+
+function Home() {
+    const [register]
+  //   const getInfo = () => {
+  //     fetch("http://localhost:4000/companies")
+  //       .then((res) => res.json())
+  //       .then((data) => console.log(data));
+  //   };
+
+  return (
+    <div>
+      <h1 className="header">Contact Me</h1>
+  
+    </div>
+  );
+}
+
+export default Home;
