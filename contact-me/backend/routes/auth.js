4b2aa59f50105e74e02320bdccd101d7260e701a
Selfmade20 <tebogo1686@gmail.com>
2020-09-14 10:30:30 +0200

    created a registerCompany route

--- contact-me/backend/routes/auth.js
+++ contact-me/backend/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const Company = require("../model/Companies");
 
 router.post("/capture", async (req, res) => {
   const user = new User({
@@ -12,6 +13,21 @@ router.post("/capture", async (req, res) => {
   try {
     const savedInfo = await user.save();
     res.send(savedInfo);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post("/registerCompany", async (req, res) => {
+  const company = new Company({
+    name: req.body.name,
+    webAddress: req.body.webAddress,
+    physicalAddress: req.body.physicalAddress,
+    email: req.body.email,
+  });
+  try {
+    const savedCompany = await company.save();
+    res.send(savedCompany);
   } catch (error) {
     res.status(400).send(error);
   }
