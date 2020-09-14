7b66f7b7c398cfd6e855cabdf57b63855e5631c2
Selfmade20 <tebogo1686@gmail.com>
2020-09-14 09:09:33 +0200

    Created app

---
+++ contact-me/backend/model/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+  },
+  surname: {
+    type: String,
+    required: true,
+    min: 3,
+  },
+  mobileNumber: {
+    type: Number,
+    required: true,
+    min: 10,
+  },
+  email: {
+    type: String,
+    required: true,
+    min: 6,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
