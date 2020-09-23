1e0aa4f7f37bdca655c1b84aa2d798b7652708f9
Selfmade20 <tebogo1686@gmail.com>
2020-09-23 10:46:05 +0200

    changed schema username to email property

--- contact-me/back-end/model/admin.js
+++ contact-me/back-end/model/admin.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema({
-  username: { type: String, required: true, min: 3, trim: true },
-  password: { type: String, required: true, min: 4, trim: true },
+  email: { type: String, required: true, trim: true },
+  password: { type: String, required: true, trim: true },
 });
 
 module.exports = mongoose.model("Admin", adminSchema);
