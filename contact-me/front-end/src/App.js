635d24d3f7fd4df17527ad22b8f6434a0018c584
Selfmade20 <tebogo1686@gmail.com>
2020-09-23 11:24:21 +0200

    Using react-router to navigate components

--- contact-me/front-end/src/App.js
+++ contact-me/front-end/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div className="app">
-    
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route exact path="/">
+            <Navbar />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
