dafce2ce9eb624cfed35c5a66b569aad15cb50b6
Selfmade20 <tebogo1686@gmail.com>
2020-09-23 11:23:36 +0200

    debugging the login route

--- contact-me/back-end/routes/admin.js
+++ contact-me/back-end/routes/admin.js
@@ -38,11 +38,11 @@ router.post("/login", async (req, res) => {
   const { error } = loginValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const user = await Admin.findOne({ username: req.body.username });
+  const user = await Admin.findOne({ email: req.body.email });
   if (!user) return res.status(400).send("Email or Password is incorrect");
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (validPassword)
+  if (!validPassword)
     return res.status(400).send("Email or Password is incorrect");
   res.send("Logged IN");
 });
