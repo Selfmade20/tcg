67cf2d2054ceaab62421a313ddfa4c06ae309e1a
Selfmade20 <tebogo1686@gmail.com>
2020-09-23 14:18:15 +0200

    name changes

--- contact-me/front-end/src/redux/reducers/index.js
+++ contact-me/front-end/src/redux/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
-import companiesReducer from "./companiesReducer";
+import companiesReducer from "./companyReducer";
 
 export const rootReducer = combineReducers({
   userReducer,
