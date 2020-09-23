6fc13dc48668afb568cca39a71b9bb92f68698ea
Selfmade20 <tebogo1686@gmail.com>
2020-09-23 08:49:42 +0200

    setup connection to database

--- contact-me/back-end/server.js
+++ contact-me/back-end/server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 dotenv.config();
+mongoose.connect(
+  process.env.ATLAS_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Successfully connected to mongoDB database!");
+  }
+);
 
 app.use(express());
 app.use(cors());
 
 const port = process.env.PORT;
-
 app.listen(port, console.log(`Server successfully running or port: ${port}`));
