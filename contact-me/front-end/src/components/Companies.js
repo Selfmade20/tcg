5946c03557d27067a9fd188d35707401f3960709
Selfmade20 <tebogo1686@gmail.com>
2020-09-23 14:46:52 +0200

    Mapping the data from the api

--- contact-me/front-end/src/components/Companies.js
+++ contact-me/front-end/src/components/Companies.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./Companies.css";
 
 function Companies() {
   const [companies, setCompanies] = useState([]);
@@ -16,10 +17,18 @@ function Companies() {
       });
   }, []);
   return (
-    <div>
-      <ul>
+    <div className="container">
+      <ul className="grid">
         {companies.map((company) => (
-          <li key={company.id}>{company.name} </li>
+          <li key={company.name}>
+            {" "}
+            <h1>Company Name: {company.name}</h1>{" "}
+            <h4>Address: {company.physicalAddress}</h4>{" "}
+            <h3> Email: {company.emailAddress}</h3>{" "}
+            <h3>Website Address: {company.websiteAddress}</h3>{" "}
+            <h3>Phone Number: {company.phoneNumber}</h3>
+            <button>Select</button>
+          </li>
         ))}
       </ul>
     </div>
