e51fa97120987972b6112bbd6248f390b6cfee66
Selfmade20 <tebogo1686@gmail.com>
2020-12-01 15:38:31 +0200

    Created mock data function for linegraphy

--- trading/src/components/LineGraph.js
+++ trading/src/components/LineGraph.js
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
+import './LineGraph.css'
 
 function LineGraph() {
   const [graphData, setGraphData] = useState([]);
-  const data = [
-    { x: 10, y: 20 },
-    { x: 15, y: 10 },
-    { x: 12, y: 4 },
-  ];
+
+  const createMockData = () => {
+    let data = [];
+    let value = 50;
+    for (var i = 0; i < 366; i++) {
+      let date = new Date();
+      date.setHours(0, 0, 0, 0);
+      date.setDate(i);
+      value += Math.round((Math.random() < 0.5 ? 1 : 0) * Math.random() * 10);
+      data.push({ x: date, y: value });
+    }
+    setGraphData(data);
+  };
+
+  useEffect(() => {
+    createMockData();
+  }, []);
   return (
     <div className="linegraph">
       <Line
@@ -15,7 +28,7 @@ function LineGraph() {
           datasets: [
             {
               type: "line",
-              data: data,
+              data: graphData,
               backgroundColor: "black",
               borderColor: "#5AC53B",
               borderWidth: 2,
@@ -29,6 +42,7 @@ function LineGraph() {
           ],
         }}
         options={{
+            // maintainAspectRatio: false,
           legend: {
             display: false,
           },
@@ -37,6 +51,18 @@ function LineGraph() {
             intersect: false,
           },
           scales: {
+            xAxes: [
+              {
+                type: "time",
+                time: {
+                  format: "MM/DD/YY",
+                  tooltipFormat: "ll",
+                },
+                ticks: {
+                  display: false,
+                },
+              },
+            ],
             yAxes: [
               {
                 ticks: {
