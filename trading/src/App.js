9003a06ce0e74408e28e95936f0557caa7132d4b
Selfmade20 <tebogo1686@gmail.com>
2020-12-01 11:12:27 +0200

    Template cleanup

---
+++ trading/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="App">
+      <div className="app__header">
+        <Header />
+      </div>
+      <div className="app__body"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ trading/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header__wrapper">
+      <div className="header__logo">Test</div>
+    </div>
+  );
+}
+
+export default Header;
