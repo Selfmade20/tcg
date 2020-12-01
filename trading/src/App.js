8434837c89c17bf4346db5bd5fd2127eb7c2f17a
Selfmade20 <tebogo1686@gmail.com>
2020-12-01 12:34:47 +0200

    Importing components

--- trading/src/App.js
+++ trading/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Header from "./components/Header";
+import Newsfeed from "./components/Newsfeed";
+import Stats from "./components/Stats";
 function App() {
   return (
     <div className="App">
       <div className="app__header">
         <Header />
       </div>
-      <div className="app__body"></div>
+      <div className="app__body">
+        <div className="app__container">
+          <Newsfeed />
+          <Stats />
+        </div>
+      </div>
     </div>
   );
 }
